234da436f5fd51aefd7b03f15b2b8c0761de27c8
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-11-10 23:27:43 +0900

    :sparkles: Added change colors when btn was clicked

--- 01_Color-flipper/app.js
+++ 01_Color-flipper/app.js
@@ -1,1 +1,35 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// Get element #btn
+const btn = document.getElementById("btn");
+// Get selector .color
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  // Get random number between 0 - 3
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+// const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
+
+// btn.addEventListener("click", function () {
+//   const randomNumber = getRandomNumber();
+//   // console.log(randomNumber);
+
+//   document.body.style.backgroundColor = colors[randomNumber];
+//   color.textContent = colors[randomNumber];
+// });
+
+// function getRandomNumber() {
+//   return Math.floor(Math.random() * colors.length);
+//}
