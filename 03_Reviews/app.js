0266f2c9b8dbeec200a0582f9d4faf9d7bd90427
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-11-13 10:45:37 +0900

    :+1: Added next function

--- 03_Reviews/app.js
+++ 03_Reviews/app.js
@@ -49,13 +49,36 @@ const nextBtn = document.querySelector(".next-btn");
 const random = document.querySelector("random-btn");
 
 // set starting item
-let currentItem = 4;
+let currentItem = 0;
 
 window.addEventListener('DOMCountentdLoaded', function () {
+  showPerson(currentItem);
+})
+
+// show person based on item
+function showPerson(person) {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+
+  }
+  showPerson(currentItem);
 })
 
+// show prev person
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
