f40bde3ad198c5c8ffdfb864bf1fcf568e08304e
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-11-14 20:43:00 +0900

    :sparkles: Added Variables to display what times on console

--- 12_Countdown-timer/app.js
+++ 12_Countdown-timer/app.js
@@ -21,3 +21,39 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date();
+// console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+// console.log(months[month]);
+const date = futureDate.getDay();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${year} ${hours} ${date} ${month} ${year}: ${minutes}`;
+console.log(giveaway.textContent);
+
+// feture time in ms
+const futureTime = futureDate.getTime();
+console.log(futureTime);
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60min
+  // 1d = 24hr
+}
+
+getRemainingTime();
